cef32592efec816163a4ec04ca0041e549455b0c
borehelu <borehelu@gmail.com>
2023-02-28 22:43:57 +0300

    server configuration

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 import express from "express";
+import http from "http";
 import cors from "cors";
+import { Server } from "socket.io";
 import { config } from "dotenv";
 import apiRoutes from "./routes/index.js";
 
 config();
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 app.use(cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", apiRoutes);
 
-app.listen(5000, () => {
+io.on("connection", (socket) => {
+  console.log("client connected", socket.id);
+
+  socket.on("add_idea", (data) => {
+    socket.emit("new_idea", data);
+  });
+
+  socket.on("upvote_idea", (data) => {
+    socket.emit("new_idea", data);
+  });
+});
+
+server.listen(5000, () => {
   console.log("listening");
 });
